31221b0f21aeba96b7fea35a7970c46830d9cdd0
Hikari7 <kobe.hikari@moneyforward.co.jp>
2024-11-29 10:22:54 +0900

    update: modify to test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-const newFunction2 = (msg) => { console.log(msg || 'empty')};
+const newFunction2 = (msg) => {
+  console.log(msg || 'empty');
+};
 
-newFunction('hello world 35');
+newFunction('hello world 4');
